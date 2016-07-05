6fde76db94c3f718c64e6b89d13bec1f6801e752
richardpitargue <jlpitargue@up.edu.ph>
2016-07-05 10:57:09 +0800

    Update route to /waiss

--- client/modules/farmer-dashboard/actions/utility.js
+++ client/modules/farmer-dashboard/actions/utility.js
@@ -2,6 +2,28 @@ export default {
     selectPlantingSeason(context, plantingSeasonId) {
         const {FlowRouter} = context;
 
-        FlowRouter.go('/farmer/' + plantingSeasonId);
+        FlowRouter.go('/waiss/' + plantingSeasonId);
+    },
+    formatDate(context, date) {
+        const theDate = new Date(date);
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+        const month = theDate.getMonth();
+        const day = theDate.getDate().toString().slice(0,1) == '0' ? theDate.getDate().toString().slice(1) : theDate.getDate();
+        const year = theDate.getFullYear();
+        
+        return months[month] + ' ' + day + ', ' + year;
     }
 }
